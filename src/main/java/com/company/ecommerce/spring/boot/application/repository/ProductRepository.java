bd183832a2181bb91a838a1ea6271daefbe163d1
RAMI <ramiabdelilah1@gmail.com>
2020-09-24 10:56:24 +0200

    add spring data rest

--- src/main/java/com/company/ecommerce/spring/boot/application/repository/ProductRepository.java
+++ src/main/java/com/company/ecommerce/spring/boot/application/repository/ProductRepository.java
@@ -2,6 +2,8 @@ package com.company.ecommerce.spring.boot.application.repository;
 
 import com.company.ecommerce.spring.boot.application.domain.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource(collectionResourceRel = "products", path = "products")
 public interface ProductRepository extends JpaRepository<Product, Long> {
 }
